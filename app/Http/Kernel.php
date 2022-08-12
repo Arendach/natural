b8de616a87ed33ce9a2f548ffe3eae0c47317209
Arendach Taras <t.arendach@magneticone.com>
2022-08-12 20:23:08 +0300

    fix namespace

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\EncryptCookies;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Middleware\TrimStrings;
 use App\Http\Middleware\TrustProxies;
 use App\Http\Middleware\VerifyCsrfToken;
